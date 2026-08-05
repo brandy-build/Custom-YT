fa9499ea0d69baeac862d221ac29fd9bc01081e5
AGR Project <chawda7prajwal@gmail.com>
2026-08-05 20:20:36 +0530

    ci: update rust.yml workflow and fix release pipeline

---
+++ target/debug/deps/custom_yt-54c2971e2a920e48.d
@@ -0,0 +1,10 @@
+C:\Users\prajw\Programs\Custom YT\Custom-YT\target\debug\deps\custom_yt-54c2971e2a920e48.d: src\main.rs src\../ascii-art.txt Cargo.toml
+
+C:\Users\prajw\Programs\Custom YT\Custom-YT\target\debug\deps\libcustom_yt-54c2971e2a920e48.rmeta: src\main.rs src\../ascii-art.txt Cargo.toml
+
+src\main.rs:
+src\../ascii-art.txt:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
